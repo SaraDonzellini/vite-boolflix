d99fb245ad33ab74eab21d76e16e81c1ec0a6f11
SaraDonzellini <saradonzie@gmail.com>
2023-01-13 13:39:42 +0100

    starting exercise

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
-<script setup>
+<script>
 
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    methods: {
+        searchApi() {
+            axios.get("https://api.themoviedb.org/3/search/movie?api_key=4bc85001b5721a0ecbd19f8488e3f941&", {
+                params: {
+                    language: "en-US",
+                    page: 1,
+                    include_adult = false,
+                }
+            })
+                .then(function (response) {
+                console.log(response);
+            })
+                .catch(function (error) {
+                console.log(error);
+            });
+        }
+    },
+    components: { 
+      AppHeader 
+    }
+}
 </script>
 
 <template>
+  <header>
+    <AppHeader />
+  </header>
 </template>
 
 <style>
+
+
 </style>
