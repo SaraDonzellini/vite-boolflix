d594e13d6388fc7eb4c58f4a7581ef2ef5e97e17
SaraDonzellini <saradonzie@gmail.com>
2023-01-16 11:01:12 +0100

    still trying  to add flags

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,12 +16,28 @@ export default {
 <template>
   <section class="container">
     <div class="row">
+      <h2>Movies</h2>
       <div class="col-4" v-for="movie in store.searchedMovie">
-        {{ movie.title }} - {{ movie.original_title }} - {{ movie.original_language }} - {{ movie.vote_average }}
+        <h4>
+          {{ movie.title }}
+        </h4>
+        <h5>
+          <em>{{ movie.original_title }}</em>
+        </h5>
+        <p>Language: <span class='fi fi-{{movie.original_language}}'></span> </p>
+        <p>Rating: {{ movie.vote_average }}</p>
 
       </div>
+      <h2>Tv shows</h2>
       <div class="col-4" v-for="tvShow in store.searchedTvshow">
-        {{ tvShow.name }} - {{ tvShow.original_name }} - {{ tvShow.original_language }} - {{ tvShow.vote_average }}
+        <h4>
+          {{ tvShow.name }}
+        </h4>
+        <h5>
+          <em>{{ tvShow.original_name }}</em>
+        </h5>
+        <p>Language: <span class='fi fi-{{tvShow.original_language}}'></span> </p>
+        <p>Rating: {{ tvShow.vote_average }}</p>
 
       </div>
 
