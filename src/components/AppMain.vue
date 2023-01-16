75e00129f703d4fc8a565ef2deb6271d9bad651c
SaraDonzellini <saradonzie@gmail.com>
2023-01-16 11:24:04 +0100

    added flags

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,12 @@
 <script>
 import { store } from '../store.js'
-import "/node_modules/flag-icons/css/flag-icons.min.css";
+import LangFlag from 'vue-lang-code-flags';
 
 export default {
   name: 'AppMain',
+  components : {
+    LangFlag,
+  },
   data() {
     return {
       store,
@@ -16,6 +19,7 @@ export default {
 <template>
   <section class="container">
     <div class="row">
+
       <h2>Movies</h2>
       <div class="col-4" v-for="movie in store.searchedMovie">
         <h4>
@@ -24,10 +28,10 @@ export default {
         <h5>
           <em>{{ movie.original_title }}</em>
         </h5>
-        <p>Language: <span class='fi fi-{{movie.original_language}}'></span> </p>
+        <p>Language: <lang-flag :iso="movie.original_language" /></p>
         <p>Rating: {{ movie.vote_average }}</p>
-
       </div>
+
       <h2>Tv shows</h2>
       <div class="col-4" v-for="tvShow in store.searchedTvshow">
         <h4>
@@ -36,9 +40,8 @@ export default {
         <h5>
           <em>{{ tvShow.original_name }}</em>
         </h5>
-        <p>Language: <span class='fi fi-{{tvShow.original_language}}'></span> </p>
+        <p>Language: <lang-flag :iso="tvShow.original_language" /> </p>
         <p>Rating: {{ tvShow.vote_average }}</p>
-
       </div>
 
     </div>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import './assets/styles/style.scss'
 import App from './App.vue'
 import * as bootstrap from 'bootstrap'
+import LangFlag from 'vue-lang-code-flags';
 
+// Vue.component('lang-flag', LangFlag);
 createApp(App).mount('#app')
