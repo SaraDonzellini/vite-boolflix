61e2554adc3a1b8b6a5cf4084351b7276959060a
SaraDonzellini <saradonzie@gmail.com>
2023-01-17 13:26:30 +0100

    removed methodsfrom  components

--- src/components/AppActorsMovie.vue
+++ src/components/AppActorsMovie.vue
@@ -10,29 +10,10 @@ export default {
   data() {
     return {
       store,
-      apiKey: "4bc85001b5721a0ecbd19f8488e3f941",
-      apiUri: "https://api.themoviedb.org/3/"
+
     }
   },
-  methods: {
-    searchActorsMovie(movieId) {
-      axios.get(this.apiUri + 'movie/' + movieId + '/credits', {
-        params: {
-          api_key: this.apiKey,
-        }
-      })
-        .then((response) => {
-          console.log(response.data.cast);
-          this.store.searchedCastMovie = response.data.cast;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    },
-    
-    
-
-  },
+  
 }
 </script>
 
--- src/components/AppActorsTV.vue
+++ src/components/AppActorsTV.vue
@@ -10,29 +10,11 @@ export default {
   data() {
     return {
       store,
-      apiKey: "4bc85001b5721a0ecbd19f8488e3f941",
-      apiUri: "https://api.themoviedb.org/3/"
     }
   },
-  methods: {
-    searchActorsTvShows(seriesId) {
-      axios.get(this.apiUri + 'tv/' + seriesId + '/credits', {
-        params: {
-          api_key: this.apiKey,
-        }
-      })
-        .then((response) => {
-          console.log(response.data.cast);
-          this.store.searchedCastTV = response.data.cast;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    },
-    
 
-  },
 }
+
 </script>
 
 <template>
